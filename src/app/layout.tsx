aa374f47bf554ad5ca32d5ad535a586c23b269eb
SGH-SS <ssrin043@ucr.edu>
2025-09-24 17:51:41 -0700

    nigger commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
-      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+      { url: "/favicon-48x48-bk1.png", sizes: "48x48", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" }
     ],
     shortcut: ["/favicon.ico"],
