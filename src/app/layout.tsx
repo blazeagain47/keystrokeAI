19d7a02cff0aad7a9470a43907a92d1dbacbb322
SGH-SS <ssrin043@ucr.edu>
2025-09-24 17:35:06 -0700

    locked in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export const metadata: Metadata = {
       { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" }
     ],
+    shortcut: ["/favicon.ico"],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
     other: [
       {
