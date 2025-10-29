6a6e6912be0666048b4c216ecdf7b7b78a707018
SGH-SS <ssrin043@ucr.edu>
2025-10-28 18:12:29 -0700

    meta verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,10 +56,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7952887767155151" />
         <Script
           id="adsense-verify"
-          async
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7952887767155151"
           crossOrigin="anonymous"
         />
